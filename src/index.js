61467b45ab7f2dcd6406e5d9baaeffe598f2f7a4
chakri.realnex <chakri.realnex@gmail.com>
2022-01-30 12:51:40 +0700

    build

---
+++ src/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+exports.__esModule = true;
+require("../type/index");
+var date_fns_be_utils_1 = require("./date-fns-be-utils");
+__createBinding(exports, date_fns_be_utils_1, "default");
